a6833d8d93712ae2571dc57750fb5159974495c9
GauravJawalkar <gauravjawalkar8@gmail.com>
2025-01-29 22:08:06 +0530

    name description in the product model

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -2,7 +2,18 @@ import mongoose, { Schema } from "mongoose";
 
 const productSchema = new Schema(
     {
-
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+            lowercase: true,
+        },
+        description: {
+            type: String,
+            required: true,
+            trim: true,
+            lowercase: true,
+        },
     },
     {
         timestamps: true
