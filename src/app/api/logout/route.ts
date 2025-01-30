346658927c7ab922a3d9cc9b75f9dc74248c5274
GauravJawalkar <gauravjawalkar8@gmail.com>
2025-01-30 22:54:31 +0530

    fixed the logout bug and setting the refreshToken as empty when loggedOut

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,7 +1,29 @@
-import { NextResponse } from "next/server";
+import { auth } from "@/middlewares/auth.middleware";
+import { User } from "@/models/user.model";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(request: NextRequest) {
     try {
+
+        // getting the user by finding it from the accessToken by decoding it through auth()
+        const reqUser = await auth(request);
+
+        if (!reqUser) {
+            return NextResponse.json({ error: "No user found" })
+        }
+
+        // collecting the id form the reqUser got from the auth() function and finding it in database, setting it's refreshToken as "" 
+        await User.findByIdAndUpdate(reqUser?._id,
+            {
+                $set: {
+                    refreshToken: "",
+                }
+            },
+            {
+                new: true
+            }
+        )
+
         const response = NextResponse.json({
             message: "Logout Successful",
             success: true
@@ -15,6 +37,7 @@ export async function GET() {
         return response;
 
     } catch (error) {
+        console.log(error)
         return NextResponse.json({ error: error }, { status: 500 })
     }
 }
