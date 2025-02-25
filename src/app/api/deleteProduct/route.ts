33fd06b70e618a51e4ef1be9d16898f9c0205909
GauravJawalkar <gauravjawalkar8@gmail.com>
2025-02-25 22:25:04 +0530

    delete product api and frontend done

--- src/app/api/deleteProduct/route.ts
+++ src/app/api/deleteProduct/route.ts
@@ -1,8 +1,41 @@
+import connectDB from "@/db/dbConfig";
+import { Product } from "@/models/product.model";
+import { User } from "@/models/user.model";
 import { NextRequest, NextResponse } from "next/server";
 
 
+connectDB();
+
 export async function DELETE(request: NextRequest) {
     try {
+
+        const reqBody = await request.json();
+
+        const { productId, sellerId } = reqBody;
+
+        if (!productId || !sellerId) {
+            return NextResponse.json({ error: "productId and sellerId are required" }, { status: 401 })
+        }
+
+        const user = await User.findById(sellerId);
+
+        if (!user) {
+            return NextResponse.json({ error: "Failed to find the seller" }, { status: 404 })
+        }
+
+        const isSeller = user?.isSeller;
+
+        if (!isSeller) {
+            return NextResponse.json({ error: "Can't delete the product! Youre are unauthorized" }, { status: 402 })
+        }
+
+        const deletedProduct = await Product.findByIdAndDelete(productId);
+
+        if (!deletedProduct) {
+            return NextResponse.json({ error: "Failed to delete the product" }, { status: 402 })
+        }
+
+        return NextResponse.json({ product: "Product Deletes Successfully" }, { status: 200 })
 
     } catch (error) {
         return NextResponse.json({ error: "Failed to delete the product" }, { status: 500 })
--- src/components/MyProducts.tsx
+++ src/components/MyProducts.tsx
@@ -9,6 +9,8 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import { PenLine, Trash } from 'lucide-react';
 import EditDetailsModal from './EditDetailsModal';
+import toast from 'react-hot-toast';
+import Loader from './Loader';
 
 const MyProducts = ({ sellerId, load }: { sellerId: string, load: boolean }) => {
 
@@ -20,6 +22,7 @@ const MyProducts = ({ sellerId, load }: { sellerId: string, load: boolean }) => 
     const [oldDiscount, setOldDiscount] = useState("");
     const [oldSize, setOldSize] = useState("");
     const [currentId, setCurrentId] = useState("");
+    const [deleteLoader, setDeleteLoader] = useState(false);
 
     async function getSellerProducts() {
         try {
@@ -30,84 +33,117 @@ const MyProducts = ({ sellerId, load }: { sellerId: string, load: boolean }) => 
         }
     }
 
+
+    const handelDelete = async (productId: string) => {
+
+        console.log("The product id is : ", productId)
+
+        try {
+            setDeleteLoader(true)
+            const response = await axios.delete('/api/deleteProduct', { data: { sellerId, productId } })
+
+            if (response.data.product) {
+                toast.success("Product Deleted");
+            }
+
+            setDeleteLoader(false);
+
+            getSellerProducts();
+
+        } catch (error) {
+            setDeleteLoader(false)
+            toast.error("Failed to delete the product. Try Again")
+            console.log(error)
+        } finally {
+            setDeleteLoader(false)
+        }
+    }
+
     useEffect(() => {
         getSellerProducts()
     }, [load])
 
 
     return (
-        <div className='grid grid-cols-4 py-10 gap-10'>
+        <>
             {
-                prodData.length === 0 ? "No Products Found" :
-
-                    prodData.map(
-                        ({ _id, name, description, images, price, discount, size }: any) => {
-                            return (
-                                <div key={_id} className='border p-5 dark:border-gray-500'>
-                                    <div className=''>
-                                        <Swiper modules={[EffectFade, Pagination]}
-                                            pagination={{ clickable: true }} spaceBetween={50} effect="card" className='border rounded dark:border-gray-500'>
-                                            {images.map((elem: string, index: Key | null | undefined) => {
-                                                return (
-                                                    <SwiperSlide key={index} className=''>
-                                                        <Image src={elem} loading='lazy' alt='image prod' width={300} height={200} className='h-[300px] w-full object-cover rounded' />
-                                                    </SwiperSlide>
-                                                )
-                                            })}
-                                        </Swiper>
-                                        <h1 className='capitalize text-xl font-bold antialiased py-2 line-clamp-2'>
-                                            {name}
-                                        </h1>
-                                        <p className='text-base text-gray-500 line-clamp-3'>
-                                            {description}
-                                        </p>
-                                        <div className='py-2 flex items-center gap-5 justify-between'>
-                                            <h1 className='font-light '>
-                                                <span className='font-semibold'> Price </span> ₹ {price}
-                                            </h1>
-                                            <h1 className='font-light '>
-                                                <span className='font-semibold'>  Discount </span> ₹ {discount}
-                                            </h1>
+                prodData.length === 0 ?
+                    <div className='text-center w-full py-10 animate-pulse font-semibold text-lg antialiased'>
+                        No Products Found
+                    </div> :
+                    <div className='grid grid-cols-4 py-10 gap-10'>
+                        {
+                            prodData.map(
+                                ({ _id, name, description, images, price, discount, size }: any) => {
+                                    return (
+                                        <div key={_id} className='border p-5 dark:border-gray-500'>
+                                            <div className=''>
+                                                <Swiper modules={[EffectFade, Pagination]}
+                                                    pagination={{ clickable: true }} spaceBetween={50} effect="card" className='border rounded dark:border-gray-500'>
+                                                    {images.map((elem: string, index: Key | null | undefined) => {
+                                                        return (
+                                                            <SwiperSlide key={index} className=''>
+                                                                <Image src={elem} loading='lazy' alt='image prod' width={300} height={200} className='h-[300px] w-full object-cover rounded' />
+                                                            </SwiperSlide>
+                                                        )
+                                                    })}
+                                                </Swiper>
+                                                <h1 className='capitalize text-xl font-bold antialiased py-2 line-clamp-2'>
+                                                    {name}
+                                                </h1>
+                                                <p className='text-base text-gray-500 line-clamp-3'>
+                                                    {description}
+                                                </p>
+                                                <div className='py-2 flex items-center gap-5 justify-between'>
+                                                    <h1 className='font-light '>
+                                                        <span className='font-semibold'> Price </span> ₹ {price}
+                                                    </h1>
+                                                    <h1 className='font-light '>
+                                                        <span className='font-semibold'>  Discount </span> ₹ {discount}
+                                                    </h1>
+                                                </div>
+                                                <div className='font-light '>
+                                                    <span className='font-semibold'>  Total </span> ₹ {price - discount}
+                                                </div>
+                                                <div className='py-2 flex items-center gap-3'>
+                                                    <button className='px-3 py-1 bg-green-500 hover:bg-green-700 transition-colors ease-in-out duration-200 rounded text-white flex items-center justify-center gap-2' onClick={() => {
+                                                        setEditModal(true)
+                                                        setOldName(name);
+                                                        setOldPrice(price);
+                                                        setOldDescripion(description);
+                                                        setOldDiscount(discount);
+                                                        setOldSize(size);
+                                                        setCurrentId(_id);
+                                                    }}>
+                                                        Edit
+                                                        <span><PenLine className='h-4 w-4' /></span>
+                                                    </button>
+                                                    <button className='px-3 py-1 bg-red-500 hover:bg-red-700 transition-colors ease-in-out duration-200 rounded text-white flex items-center justify-center gap-2'
+                                                        onClick={() => { handelDelete(_id) }}>
+                                                        Delete
+                                                        <span><Trash className='h-4 w-4' /></span>
+                                                    </button>
+                                                </div>
+                                            </div>
+                                            <EditDetailsModal
+                                                isVisible={editModal}
+                                                onClose={() => { return setEditModal(false) }}
+                                                oldName={oldName}
+                                                oldDescripion={oldDescripion}
+                                                oldPrice={oldPrice}
+                                                oldDiscount={oldDiscount}
+                                                oldSize={oldSize}
+                                                id={currentId}
+                                                reRender={() => { return getSellerProducts() }}
+                                            />
                                         </div>
-                                        <div className='font-light '>
-                                            <span className='font-semibold'>  Total </span> ₹ {price - discount}
-                                        </div>
-                                        <div className='py-2 flex items-center gap-3'>
-                                            <button className='px-3 py-1 bg-green-500 hover:bg-green-700 transition-colors ease-in-out duration-200 rounded text-white flex items-center justify-center gap-2' onClick={() => {
-                                                setEditModal(true)
-                                                setOldName(name);
-                                                setOldPrice(price);
-                                                setOldDescripion(description);
-                                                setOldDiscount(discount);
-                                                setOldSize(size);
-                                                setCurrentId(_id);
-                                            }}>
-                                                Edit
-                                                <span><PenLine className='h-4 w-4' /></span>
-                                            </button>
-                                            <button className='px-3 py-1 bg-red-500 hover:bg-red-700 transition-colors ease-in-out duration-200 rounded text-white flex items-center justify-center gap-2'>
-                                                Delete
-                                                <span><Trash className='h-4 w-4' /></span>
-                                            </button>
-                                        </div>
-                                    </div>
-                                    <EditDetailsModal
-                                        isVisible={editModal}
-                                        onClose={() => { return setEditModal(false) }}
-                                        oldName={oldName}
-                                        oldDescripion={oldDescripion}
-                                        oldPrice={oldPrice}
-                                        oldDiscount={oldDiscount}
-                                        oldSize={oldSize}
-                                        id={currentId}
-                                        reRender={() => { return getSellerProducts() }}
-                                    />
-                                </div>
+                                    )
+                                }
                             )
                         }
-                    )
+                    </div>
             }
-        </div>
+        </>
     )
 }
 
