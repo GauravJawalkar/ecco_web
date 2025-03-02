34de5ca1f7ab9e749a9c8e5ad63253b97f9cd323
GauravJawalkar <gauravjawalkar8@gmail.com>
2025-03-02 23:07:48 +0530

    user details info frontend tweak for email verification

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,7 +2,7 @@
 
 import UserInfoCard from "@/components/UserInfoCard";
 import { useUserStore } from "@/store/UserStore";
-import { LogOut, Trash2 } from "lucide-react";
+import { LogOut, Pencil, ShieldCheck, ShieldQuestion, Trash2 } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React, { MouseEvent } from "react";
@@ -62,7 +62,33 @@ const Home = () => {
                             </div>
                         </div>
                         <UserInfoCard dataLength={dataLength} dataValue={data.name} cardTitle={'name'} />
-                        <UserInfoCard dataLength={dataLength} dataValue={data.email} cardTitle={"email"} />
+                        <div className="p-5 mb-4 border-slate-300 dark:border-[#5a5a5a] font-semibold border rounded flex items-start flex-col justify-start gap-3">
+                            <div className='capitalize font-normal text-md'>{"Email"} :</div>
+                            <div className="border w-1/3 p-3 border-slate-300 dark:border-[#5a5a5a] rounded-md hover:cursor-not-allowed">
+                                <span className="text-gray-500 dark:text-gray-400 font-normal">
+                                    {dataLength !== 0 && data.email}
+                                </span>
+                            </div>
+                            <div className="flex items-center gap-3">
+                                <button className="text-red-500 border border-slate-300 hover:bg-gray-200 dark:hover:bg-[#3a3a3a] dark:border-[#5a5a5a] transition ease-in-out duration-200 px-4 py-1 rounded-sm text-base font-normal flex items-center justify-center gap-2">
+                                    <Pencil className="h-4 w-4" />
+                                    Edit
+                                </button>
+
+                                <button className="text-green-500 border border-slate-300 hover:bg-gray-200 dark:hover:bg-[#3a3a3a] dark:border-[#5a5a5a] transition ease-in-out duration-200 px-4 py-1 rounded-sm text-base font-normal flex items-center justify-center gap-2">
+                                    {dataLength !== 0 && data.isEmailVerified ?
+                                        <div className="font-normal flex items-center justify-center gap-2">
+                                            <ShieldCheck className="h-5 w-5" />
+                                            Verified
+                                        </div> :
+                                        <div className="font-normal flex items-center justify-center gap-2">
+                                            <ShieldQuestion className="h-5 w-5" />
+                                            Verify Now
+                                        </div>
+                                    }
+                                </button>
+                            </div>
+                        </div>
                     </div>
 
                     {/* Account Settings */}
