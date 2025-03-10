ee4a8a36d470c60d97e1cf665ba7b1e1f487200d
GauravJawalkar <gauravjawalkar8@gmail.com>
2025-03-10 22:12:57 +0530

    done with the getAllProducts API

---
+++ src/app/api/getAllProducts/route.ts
@@ -0,0 +1,17 @@
+import { Product } from "@/models/product.model";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET() {
+    try {
+
+        const allProducts = await Product.find();
+
+        if (!allProducts) {
+            return NextResponse.json({ error: "No products found" }, { status: 402 })
+        }
+
+        return NextResponse.json({ data: allProducts }, { status: 200 })
+    } catch (error) {
+        return NextResponse.json({ error: error }, { status: 500 })
+    }
+}
