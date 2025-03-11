c6934a6712b18852e5a34f6c72d52b917dc0918b
GauravJawalkar <gauravjawalkar8@gmail.com>
2025-03-11 23:25:11 +0530

    done building the addCustom category api

--- src/app/api/addCategory/route.ts
+++ src/app/api/addCategory/route.ts
@@ -1,3 +1,4 @@
+import { Category } from "@/models/category.model";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
@@ -10,7 +11,24 @@ export async function POST(request: NextRequest) {
             return NextResponse.json({ "error": "Category Name is required" }, { status: 401 })
         }
 
-        
+        const existingCategory = await Category.find({ categoryName });
+
+        if (existingCategory) {
+            return NextResponse.json({ "error": "Category Already Exists" }, { status: 402 })
+        }
+
+        const newCategory = await Category.create(
+            {
+                categoryName: categoryName,
+            }
+        )
+
+        if (!newCategory) {
+            return NextResponse.json({ "error": "Failed to create a new category in the database" }, { status: 405 })
+        }
+
+        return NextResponse.json({ data: categoryName }, { status: 200 })
+
     } catch (error) {
         return NextResponse.json({ "error": error }, { status: 500 })
     }
