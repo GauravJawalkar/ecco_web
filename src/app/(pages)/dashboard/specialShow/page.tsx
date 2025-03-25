9b05d3a8b85ee56814eac54df80cef54102d3a39
GauravJawalkar <gauravjawalkar8@gmail.com>
2025-03-25 21:58:42 +0530

    developing the UI for special appearence

--- src/app/(pages)/dashboard/specialShow/page.tsx
+++ src/app/(pages)/dashboard/specialShow/page.tsx
@@ -42,51 +42,57 @@ const SpecialShowCase = () => {
             <div className='flex items-center justify-center py-10'>
                 <h1 className='text-3xl font-semibold capitalize'>set Products for Special Appearence</h1>
             </div>
+            <div className='grid grid-cols-10 place-items-start w-full border px-4 py-2'>
+                <div className='col-span-2'>Name</div>
+                <div className='col-span-1'>Images</div>
+                <div className='col-span-1'>Price</div>
+                <div className='col-span-1'>Discount</div>
+                <div className='col-span-1'>Avatar</div>
+                <div className='col-span-1'>Verified</div>
+                <div className='col-span-2'>Seller Name</div>
+                <div className='col-span-1'>Authorize</div>
+            </div>
             {
                 data.map(({ _id, prodName, prodPrice, prodImages, seller, prodDiscount, sellerAvatar, sellerName, isSellerVerified }: mapDataProps) => {
                     return (
-                        <div key={_id} className='flex items-center justify-center gap-3 w-full'>
-                            <div>
-                                <label >Name</label>
-                                <h1>{prodName}</h1>
+                        <div key={_id} className='grid grid-cols-10 gap-4 border px-4 py-2'>
+                            <div className='col-span-2'>
+                                <h1 className='w-full line-clamp-1'>{prodName}</h1>
                             </div>
 
-                            <div>
-                                <label >Images</label>
+                            <div className='col-span-1'>
                                 {
                                     prodImages.map((img: string, index: Key | null | undefined) => {
                                         return (
                                             <div key={index}>
-                                                <Image src={img} alt='avatar' height={20} width={20} />
+                                                <Image src={img} alt='avatar' className='' height={20} width={20} />
                                             </div>
                                         )
                                     })
                                 }
                             </div>
 
-                            <div>
-                                <label >Price</label>
+                            <div className='col-span-1'>
                                 <h1>{prodPrice}</h1>
                             </div>
 
                             {/* {prodImages} */}
-                            <div>
-                                <label >Disc</label>
+                            <div className='col-span-1'>
                                 <h1>{prodDiscount}</h1>
                             </div>
 
-                            <div>
-                                <label >Avatar</label>
+                            <div className='col-span-1'>
                                 <Image src={sellerAvatar} alt='avatar' height={20} width={20} />
                             </div>
-                            <div>
-                                <label >Seller Name</label>
+                            <div className='col-span-1'>
+                                <h1>{isSellerVerified ? "✅" : "❌"}</h1>
+                            </div>
+                            <div className='col-span-1 '>
                                 <h1>{sellerName}</h1>
                             </div>
 
-                            <div>
-                                <label >Seller Verified</label>
-                                <h1>{isSellerVerified ? "✅" : "❌"}</h1>
+                            <div className='col-span-2  place-items-center'>
+                                <button className=''>Set Appearence</button>
                             </div>
                         </div>
                     )
