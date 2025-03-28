0ab4e30972c82c0a6ca597c2bc331cfa3a79bc98
GauravJawalkar <gauravjawalkar8@gmail.com>
2025-03-28 22:31:59 +0530

    building special Products Api to get special Products

--- src/components/Home/ProductShowCase.tsx
+++ src/components/Home/ProductShowCase.tsx
@@ -1,6 +1,31 @@
 "use cliet";
 
+import axios from "axios";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
 const ProductShowCase = () => {
+  const [data, setData] = useState([]);
+
+  async function getSpecialShowCaseProducts() {
+    try {
+      const response = await axios.get('../api/getSplAppReq');
+
+      if (response.data.data) {
+        setData(response.data.data)
+        console.log(data)
+        toast.success("Special Products are here")
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Failed to fetch the special products")
+    }
+  }
+
+  useEffect(() => {
+    getSpecialShowCaseProducts()
+  }, [])
+
   return (
     <div className="my-10">
       <div className="grid grid-cols-2 gap-10">
