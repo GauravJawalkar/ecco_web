fe0f086782cb2045af043cb397d5b9ef3256cd58
GauravJawalkar <gauravjawalkar8@gmail.com>
2025-03-31 22:30:18 +0530

    frontend UI setup for the product showcase component

--- src/components/Home/ProductShowCase.tsx
+++ src/components/Home/ProductShowCase.tsx
@@ -1,8 +1,17 @@
 "use cliet";
 
 import axios from "axios";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
+
+interface dataProps {
+  _id: string,
+  prodImages: [string],
+  sellerName: string,
+  prodName: string
+
+}
 
 const ProductShowCase = () => {
   const [data, setData] = useState([]);
@@ -21,6 +30,9 @@ const ProductShowCase = () => {
     }
   }
 
+  const firstTwoProducts = data.slice(0, 2);
+  const lastTwoProducts = data.slice(2, 4);
+
   useEffect(() => {
     getSpecialShowCaseProducts()
   }, [])
@@ -28,16 +40,42 @@ const ProductShowCase = () => {
   return (
     <div className="my-10">
       <div className="grid grid-cols-2 gap-10">
-        <div className="h-64 border p-5">
+        <div className="h-auto border p-5">
           <div className="grid grid-cols-2 gap-5">
-            <div className="border h-52">Product One</div>
-            <div className="border h-52">Product Two</div>
+            {
+              firstTwoProducts.length !== 0 && firstTwoProducts.map(({ _id, prodImages, sellerName, prodName }: dataProps) => {
+                return (
+                  <div key={_id} className="border p-4">
+                    <Image
+                      src={prodImages[0]}
+                      alt="splImage"
+                      height={200} width={200}
+                      className="w-full h-64 object-cover" />
+                    <h1 className="capitalize font-semibold text-lg py-2 line-clamp-1">{prodName}</h1>
+                    <h1 className="capitalize text-base text-gray-500">Seller : {sellerName}</h1>
+                  </div>
+                )
+              })
+            }
           </div>
         </div>
-        <div className="h-64 border p-5">
+        <div className="h-auto border p-5">
           <div className="grid grid-cols-2 gap-5">
-            <div className="border h-52">1</div>
-            <div className="border h-52">1</div>
+            {
+              lastTwoProducts.length !== 0 && lastTwoProducts.map(({ _id, prodImages, sellerName, prodName }: dataProps) => {
+                return (
+                  <div key={_id} className="border p-4">
+                    <Image
+                      src={prodImages[0]}
+                      alt="splImage"
+                      height={200} width={200}
+                      className="w-full h-64 object-cover" />
+                    <h1 className="capitalize font-semibold text-lg py-2 line-clamp-1">{prodName}</h1>
+                    <h1 className="capitalize text-base text-gray-500">Seller : {sellerName}</h1>
+                  </div>
+                )
+              })
+            }
           </div>
         </div>
       </div>
