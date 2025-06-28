59076288f93d9d4ac02a78ff07e8507d0d9cc2da
GauravJawalkar <gauravjawalkar8@gmail.com>
2025-06-28 08:57:11 +0530

    order status update and UI refactoring for better Order refrencing and understanding

--- src/app/(pages)/dashboard/ordersProcess/page.tsx
+++ src/app/(pages)/dashboard/ordersProcess/page.tsx
@@ -6,6 +6,7 @@ import { useUserStore } from "@/store/UserStore"
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 import { CircleCheckBig, MoveRight } from "lucide-react";
+import Image from "next/image";
 import { useState } from "react";
 import toast from "react-hot-toast";
 
@@ -54,7 +55,6 @@ const page = () => {
         updateOrderMutation.mutate({ orderDocId, orderID, processState });
     }
 
-
     const handelShowDetails = (_id: string) => {
         setShowDetails(prev => (prev === _id ? "" : _id));
     }
@@ -64,7 +64,7 @@ const page = () => {
     return (
         <section className="my-6">
             <div className="flex items-center justify-between">
-                <h1 className="uppercase font-semibold">Toatal Orders : {totalSellerOrders}</h1>
+                <h1 className="font-semibold uppercase">Toatal Orders : {totalSellerOrders}</h1>
                 <h1>Order Status</h1>
             </div>
 
@@ -83,62 +83,78 @@ const page = () => {
                 sellerOrders?.data?.map((order: any) => {
                     return (
                         order?.orders?.map((orderItem: any) => {
-                            const { _id, processingStatus } = orderItem;
+                            const { _id, processingStatus, orderName, orderImage, contactNumber, pinCode, paymentMethod, orderQuantity } = orderItem;
                             const sellerName = data?.name;
                             return (
-                                <div key={_id} className="p-3 border dark:border-neutral-700 rounded my-4 dark:bg-neutral-900/80">
+                                <div key={_id} className="p-3 my-4 border rounded dark:border-neutral-700 dark:bg-neutral-900/80">
                                     <div className="flex items-center justify-between p-3">
                                         <h1>Order Id : {_id}</h1>
                                         <div className="space-x-2">
-                                            <button onClick={() => { generateInvoice(orderItem, sellerName) }} className="py-1 px-2 rounded border dark:border-neutral-700 text-sm">
+                                            <button onClick={() => { generateInvoice(orderItem, sellerName) }} className="px-2 py-1 text-sm border rounded dark:border-neutral-700">
                                                 Generate Invoice</button>
-                                            <button className="py-1 px-2 rounded border dark:border-neutral-700 text-sm"
+                                            <button className="px-2 py-1 text-sm border rounded dark:border-neutral-700"
                                                 onClick={() => { handelShowDetails(_id) }}
                                             >{showDetails === _id ? "Hide Details" : "Show Details"}</button>
                                         </div>
                                     </div>
-                                    <div className={` items-center justify-between flex-grow gap-3 ${showDetails === _id ? "flex" : "hidden"}`}>
-                                        <div className="w-full p-3 border dark:border-neutral-700 rounded relative">
-                                            <h1 className="text-center">Order Confirmed</h1>
-                                            <div className="absolute -right-2 -top-3">
-                                                <CircleCheckBig className="text-green-500 bg-white dark:bg-[#1a1a1a]" />
+                                    <div className={`${showDetails === _id ? "block" : "hidden"} space-y-3 my-5`}>
+                                        {/* Details */}
+                                        <div className="grid grid-cols-5 gap-3">
+                                            <div className="border place-items-center dark:border-neutral-700 rounded-xl">
+                                                <Image src={orderImage || ""} alt={"order-Image"} width={200} height={200} className="w-32 h-32" />
                                             </div>
-                                        </div>
-                                        <MoveRight className="w-20 h-20" />
-
-                                        <div className="w-full p-3 border dark:border-neutral-700 rounded relative">
-                                            <h1 className="text-center">Order Processing</h1>
-                                            <div className="absolute -right-2 -top-3">
-                                                {(processingStatus === "Out For Delivery" || processingStatus === "Order Processing" || processingStatus === "Order Shipped") ? <CircleCheckBig className="text-green-500 bg-white dark:bg-[#1a1a1a]" /> :
-                                                    <button onClick={(e) => {
-                                                        e.preventDefault();
-                                                        handelOrderUpdate(order._id, _id, "Processing");
-                                                    }} className="bg-white dark:bg-[#1a1a1a] px-2 border dark:border-neutral-700 rounded text-sm">Processed ?</button>
-                                                }
+                                            <div className="flex items-center justify-center p-2 border dark:border-neutral-700 rounded-xl capitalize">{orderName}</div>
+                                            <div className="flex items-center justify-center p-2 border dark:border-neutral-700 rounded-xl">
+                                                Contact: +91 {contactNumber}
+                                                <br />
+                                                Quantity : {orderQuantity}
                                             </div>
+                                            <div className="flex items-center justify-center p-2 border dark:border-neutral-700 rounded-xl">PinCode : {pinCode}</div>
+                                            <div className="flex items-center justify-center p-2 border dark:border-neutral-700 rounded-xl">{paymentMethod === "COD" ? "Cash On Delivery" : "Online"}</div>
                                         </div>
-                                        <MoveRight className="w-20 h-20" />
-                                        <div className="w-full p-3 border dark:border-neutral-700 rounded relative">
-                                            <h1 className="text-center">Order Shipped</h1>
-                                            <div className="absolute -right-2 -top-3">
-                                                {(processingStatus === "Order Shipped" || processingStatus === "Out For Delivery") ? <CircleCheckBig className="text-green-500 bg-white dark:bg-[#1a1a1a]" /> :
-                                                    <button onClick={(e) => {
-                                                        e.preventDefault();
-                                                        handelOrderUpdate(order._id, _id, "Ship");
-                                                    }} className="bg-white dark:bg-[#1a1a1a] px-2 border dark:border-neutral-700 rounded text-sm">Shipped ?</button>
-                                                }
+                                        {/* Processing status state */}
+                                        <div className="flex items-center justify-between flex-grow gap-3">
+                                            <div className="relative w-full p-3 border rounded dark:border-neutral-700">
+                                                <h1 className="text-center">Order Confirmed</h1>
+                                                <div className="absolute -right-2 -top-3">
+                                                    <CircleCheckBig className="text-green-500 bg-white dark:bg-[#1a1a1a]" />
+                                                </div>
                                             </div>
-                                        </div>
-                                        <MoveRight className="w-20 h-20" />
-                                        <div className="w-full p-3 border dark:border-neutral-700 rounded relative">
-                                            <h1 className="text-center">Out For Delivery</h1>
-                                            <div className="absolute -right-2 -top-3">
-                                                {processingStatus === "Out For Delivery" ? <CircleCheckBig className="text-green-500 bg-white dark:bg-[#1a1a1a]" /> :
-                                                    <button onClick={(e) => {
-                                                        e.preventDefault();
-                                                        handelOrderUpdate(order._id, _id, "Deliver");
-                                                    }} className="bg-white dark:bg-[#1a1a1a] px-2 border dark:border-neutral-700 rounded text-sm">Delivered ?</button>
-                                                }
+                                            <MoveRight className="w-20 h-20" />
+                                            <div className="relative w-full p-3 border rounded dark:border-neutral-700">
+                                                <h1 className="text-center">Order Processing</h1>
+                                                <div className="absolute -right-2 -top-3">
+                                                    {(processingStatus === "Out For Delivery" || processingStatus === "Order Processing" || processingStatus === "Order Shipped") ? <CircleCheckBig className="text-green-500 bg-white dark:bg-[#1a1a1a]" /> :
+                                                        <button onClick={(e) => {
+                                                            e.preventDefault();
+                                                            handelOrderUpdate(order._id, _id, "Processing");
+                                                        }} className="bg-white dark:bg-[#1a1a1a] px-2 border dark:border-neutral-700 rounded text-sm">Processed ?</button>
+                                                    }
+                                                </div>
+                                            </div>
+                                            <MoveRight className="w-20 h-20" />
+                                            <div className="relative w-full p-3 border rounded dark:border-neutral-700">
+                                                <h1 className="text-center">Order Shipped</h1>
+                                                <div className="absolute -right-2 -top-3">
+                                                    {(processingStatus === "Order Shipped" || processingStatus === "Out For Delivery") ? <CircleCheckBig className="text-green-500 bg-white dark:bg-[#1a1a1a]" /> :
+                                                        <button onClick={(e) => {
+                                                            e.preventDefault();
+                                                            handelOrderUpdate(order._id, _id, "Ship");
+                                                        }} className="bg-white dark:bg-[#1a1a1a] px-2 border dark:border-neutral-700 rounded text-sm">Shipped ?</button>
+                                                    }
+                                                </div>
+                                            </div>
+                                            <MoveRight className="w-20 h-20" />
+                                            <div className="relative w-full p-3 border rounded dark:border-neutral-700">
+                                                <h1 className="text-center">Out For Delivery</h1>
+                                                <div className="absolute -right-2 -top-3">
+                                                    {processingStatus === "Out For Delivery" ? <CircleCheckBig className="text-green-500 bg-white dark:bg-[#1a1a1a]" /> :
+                                                        <button onClick={(e) => {
+                                                            e.preventDefault();
+                                                            handelOrderUpdate(order._id, _id, "Deliver");
+                                                        }} className="bg-white dark:bg-[#1a1a1a] px-2 border dark:border-neutral-700 rounded text-sm">Delivered ?</button>
+                                                    }
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
